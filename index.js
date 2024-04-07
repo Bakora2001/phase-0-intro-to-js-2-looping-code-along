76f66e8e1f1e1bd2a1f1e3f1e312c212e96d8a5e
Maxwel <maxwellbakora@gmail.com>
2024-04-07 18:06:12 +0300

    Added looping code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+function wrapGift(gift) {
+    console.log(`Wrapped ${gift} and added a bow!`);
+  }
+  
+  const gifts = ["teddy bear", "drone", "doll"];
+  
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      wrapGift(gifts[i]);
+    }
+  }
+  
+  wrapGifts(gifts);
+  
+  function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+  }
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const thankYouCards = writeCards(names, eventName);
+  console.log(thankYouCards);
+  
+  // Function to count down from a given number
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  countDown(10);
+  
